db3aa785900659ff369ab90240883a9ddeb8d00b
fengliang <fengliang.gridsum.com>
2023-05-25 09:09:09 +0800

    feat: 配置@别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // Specify symbolId format
+      symbolId: 'icon-[dir]-[name]',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': 'src',
+    },
+  },
 })
