89202189ee8dd43243e4244071cd9a49f9d7458c
fengliang <fengliang.gridsum.com>
2023-05-25 09:13:04 +0800

    feat: 初始化部分页面，集成element-plus，新增国际化配置，暗黑主题配置

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
-import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'virtual:svg-icons-register'
+import globalComponentsRegistry from '@/components/index.ts'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(globalComponentsRegistry)
+app.mount('#app')
+
+console.log('import.meta.env', import.meta.env)
---
+++ src/views/home/ThingsBoardHome.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <el-empty description="description" />
+</template>
