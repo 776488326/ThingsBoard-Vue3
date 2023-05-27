1cf0ca5c193d4a3541aaa327dcba86a507798b7e
fengliang <fengliang.gridsum.com>
2023-05-27 10:31:38 +0800

    chore: 修改部分依赖配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { viteMockServe } from 'vite-plugin-mock'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,19 @@ export default defineConfig({
       // Specify symbolId format
       symbolId: 'icon-[dir]-[name]',
     }),
+    viteMockServe({
+      mockPath: 'mock',
+      enable: false,
+    }),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        javascriptEnabled: true,
+        additionalData: '@import "./src/styles/variable.scss";',
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': 'src',
